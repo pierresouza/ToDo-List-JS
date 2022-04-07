254591e1e7743419971412be69f8d0b94bb5455e
pierre souza <pas@lionx.com.br>
2022-04-07 17:15:47 -0300

    update js and html functions

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,3 @@
-// const criarItem = () => {
-//   const item = document.createElement("label");
-//   item.classList.add("todo__item");
-//   item.innerHTML = `
-//   <input type="radio">
-//   <div>teste de item 1</div>
-//   <input type="button" value="X">
-//   `;
-
-//   document.getElementById("todoList").appendChild(item);
-// };
-
 function adicionar() {
   let text = document.getElementById("texto").value;
 
