31784a1f835405cef488aa29f0bb4e447ab6a97c
Pedro Henrique Lopes <89090945+Lop3sPedro@users.noreply.github.com>
2022-04-07 17:54:24 -0300

    Delete src directory

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-function adicionar() {
-  let text = document.getElementById("texto").value;
-
-  let list = document.getElementById("lista").innerHTML;
-
-  list += "<li> " + text + "</li>";
-
-  document.getElementById("lista").innerHTML = list;
-}
