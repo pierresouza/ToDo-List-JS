df7a385c1ff51c833119305f99d8d20524b2a2f7
pierre souza <pas@lionx.com.br>
2022-04-07 16:23:29 -0300

    update js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-function () {
-let [lista, setLista] = ([]);
-let [novoItem, setNovoItem] = useState("");
-
-useEffect(() => {
-setLista([])
-}, []);
-
-
-
-}
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#input-container", input).value.length == 0) {
+    alert("por favor adicione uma tarefa");
+  } else {
+    document.querySelector("#tasks").innerHTML += `<div class="task">
+    <span id="taskname">
+      ${document.querySelector("#newtask input").value}
+    </span>
+    </div>`;
+  }
+};
