73c3e49db325136d317e776e7d4fbc7409045c52
Pedro Henrique Lopes <phs@lionx.com.br>
2022-04-07 17:00:17 -0300

    update js function

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,21 @@
-document.querySelector("#push").onclick = function () {
-  if (document.querySelector("#input-container", input).value.length == 0) {
-    alert("por favor adicione uma tarefa");
-  } else {
-    document.querySelector("#tasks").innerHTML += `<div class="task">
-    <span id="taskname">
-      ${document.querySelector("#newtask input").value}
-    </span>
-    </div>`;
-  }
-};
+// const criarItem = () => {
+//   const item = document.createElement("label");
+//   item.classList.add("todo__item");
+//   item.innerHTML = `
+//   <input type="radio">
+//   <div>teste de item 1</div>
+//   <input type="button" value="X">
+//   `;
+
+//   document.getElementById("todoList").appendChild(item);
+// };
+
+function adicionar() {
+  let text = document.getElementById("texto").value;
+
+  let list = document.getElementById("lista").innerHTML;
+
+  list += "<li> " + text + "</li>";
+
+  document.getElementById("lista").innerHTML = list;
+}
