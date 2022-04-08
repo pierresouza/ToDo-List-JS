0409bb97f715968fd41b17ac7cee0c62ac5a480c
Gabriel Fratelli <gfc@lionx.com.br>
2022-04-08 11:00:07 -0300

    icons-edit

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
 const criarTarefa = (evento) => {
-  console.log(criarTarefa);
   evento.preventDefault();
 
-  const lista = document.querySelector("#data-lista");
-  const input = document.querySelector("#data-input");
+  const lista = document.querySelector("[data-lista]");
+  const input = document.querySelector("[data-input]");
   const valor = input.value;
 
   const tarefa = document.createElement("li");
@@ -23,7 +22,11 @@ const BotaoConclui = () => {
   const botaoConclui = document.createElement("button");
 
   botaoConclui.classList.add("check-button");
-  botaoConclui.innerText = "Concluir";
+  botaoConclui.innerHTML = `
+  <svg width="20" height="20" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="6" cy="6" r="5.5" stroke="#CBCBCB"/>
+  </svg>
+  `;
   botaoConclui.addEventListener("click", concluirTarefa);
   return botaoConclui;
 };
@@ -38,7 +41,12 @@ const concluirTarefa = (evento) => {
 
 const BotaoDeleta = () => {
   const botaoDeleta = document.createElement("button");
-  botaoDeleta.innerText = "deletar";
+  botaoDeleta.innerHTML = `
+  <svg width="20" height="20" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="9" cy="8.99999" r="5.5" transform="rotate(45 9 8.99999)" stroke="#EB3D3D"/>
+  <rect x="10.6971" y="6.73724" width="0.8" height="5.6" rx="0.4" transform="rotate(45 10.6971 6.73724)" fill="#EB3D3D"/>
+  <rect x="6.73726" y="7.30292" width="0.8" height="5.6" rx="0.4" transform="rotate(-45 6.73726 7.30292)" fill="#EB3D3D"/>
+  </svg>`;
   botaoDeleta.addEventListener("click", deletarTarefa);
   return botaoDeleta;
 };
@@ -51,5 +59,5 @@ const deletarTarefa = (evento) => {
   return botaoDeleta;
 };
 
-const novaTarefa = document.querySelector("#data-button");
+const novaTarefa = document.querySelector("[data-button]");
 novaTarefa.addEventListener("click", criarTarefa);
