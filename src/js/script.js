14766f759868a2300735a1b91ddb3e06a6765550
Gabriel Fratelli <gfc@lionx.com.br>
2022-04-08 17:15:29 -0300

    projeto-finalizado
    não concluido

--- src/js/script.js
+++ src/js/script.js
@@ -22,11 +22,7 @@ const BotaoConclui = () => {
   const botaoConclui = document.createElement("button");
 
   botaoConclui.classList.add("check-button");
-  botaoConclui.innerHTML = `
-  <svg width="20" height="20" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="6" cy="6" r="5.5" stroke="#CBCBCB"/>
-  </svg>
-  `;
+  botaoConclui.innerHTML = `<div class="concluir-btn"></div>`;
   botaoConclui.addEventListener("click", concluirTarefa);
   return botaoConclui;
 };
@@ -40,13 +36,9 @@ const concluirTarefa = (evento) => {
 };
 
 const BotaoDeleta = () => {
-  const botaoDeleta = document.createElement("button");
-  botaoDeleta.innerHTML = `
-  <svg width="20" height="20" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="9" cy="8.99999" r="5.5" transform="rotate(45 9 8.99999)" stroke="#EB3D3D"/>
-  <rect x="10.6971" y="6.73724" width="0.8" height="5.6" rx="0.4" transform="rotate(45 10.6971 6.73724)" fill="#EB3D3D"/>
-  <rect x="6.73726" y="7.30292" width="0.8" height="5.6" rx="0.4" transform="rotate(-45 6.73726 7.30292)" fill="#EB3D3D"/>
-  </svg>`;
+  const botaoDeleta = document.createElement("button.btn");
+  botaoDeleta.classList.add("delete-button");
+  botaoDeleta.innerHTML = `<div class="delete-btn"></div>`;
   botaoDeleta.addEventListener("click", deletarTarefa);
   return botaoDeleta;
 };
