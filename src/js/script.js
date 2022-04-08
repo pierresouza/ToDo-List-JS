563261daa6bbf582eba462f69ebfd0a21f487cc9
Gabriel Fratelli <gfc@lionx.com.br>
2022-04-08 10:02:25 -0300

    js-const

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,8 @@ const criarTarefa = (evento) => {
 
   const tarefa = document.createElement("li");
   tarefa.classList.add("task");
-  const conteudo = <p class="content">${valor}</p>;
+  const conteudo = `<p class="content">${valor}</p>`;
+  console.log(valor);
 
   tarefa.innerHTML = conteudo;
 
@@ -18,4 +19,37 @@ const criarTarefa = (evento) => {
   input.value = "";
 };
 
-//
+const BotaoConclui = () => {
+  const botaoConclui = document.createElement("button");
+
+  botaoConclui.classList.add("check-button");
+  botaoConclui.innerText = "Concluir";
+  botaoConclui.addEventListener("click", concluirTarefa);
+  return botaoConclui;
+};
+
+const concluirTarefa = (evento) => {
+  const botaoConclui = evento.target;
+
+  const tarefaCompleta = botaoConclui.parentElement;
+
+  tarefaCompleta.classList.toggle("done");
+};
+
+const BotaoDeleta = () => {
+  const botaoDeleta = document.createElement("button");
+  botaoDeleta.innerText = "deletar";
+  botaoDeleta.addEventListener("click", deletarTarefa);
+  return botaoDeleta;
+};
+
+const deletarTarefa = (evento) => {
+  const botaoDeleta = evento.target;
+  const tarefaCompleta = botaoDeleta.parentElement;
+
+  tarefaCompleta.remove();
+  return botaoDeleta;
+};
+
+const novaTarefa = document.querySelector("#data-button");
+novaTarefa.addEventListener("click", criarTarefa);
