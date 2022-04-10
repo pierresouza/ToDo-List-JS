60c906aaf31e16e8d4e25087d0d755d1139d65cb
pierre souza <65835017+pierresouza@users.noreply.github.com>
2022-04-10 19:25:14 -0300

    update functions

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,48 @@
-function adicionar() {
-  let text = document.getElementById("texto").value;
-  let list = document.getElementById("lista").innerHTML;
+var todoList = [{
+    id: 1,
+    status: true,
+    description: "",
+}]
 
-  list += "<li>" + text + "</li>";
-  document.getElementById("lista").innerHTML = list;
-  document.getElementById("texto").value = null;
+function addNewItem() {
+    var inputValue = document.getElementById(`insert`)
+    console.log(`Adicionou`, inputValue.value)
+
+    var listItems = document.getElementById(`list-items`)
+    var li = document.createElement(`li`)
+    var input = document.createElement(`input`)
+    var text = document.createElement(`p`)
+    var button = document.createElement(`button`)
+    var img = document.createElement(`img`)
+
+
+
+    input.setAttribute(`type`, `checkbox`)
+    button.appendChild(img)
+
+    text.innerHTML = inputValue
+    li.appendChild(input)
+    li.appendChild(text)
+    li.appendChild(button)
+
+    listItems.appendChild(li)
+
+    todoList.push({
+        id: Math.random(),
+        status: false,
+        description: insertInput.value
+    })
+
+    console.log(todoList)
+
+    insertInput.value = ''
+
+}
+
+function RemoveItem(id) {
+    var newTodoList = todoList.filter(item => item.id != id)
+    console.log(`removeu`, id)
+
+
+
 }
